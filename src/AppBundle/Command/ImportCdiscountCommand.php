4215dfee579bd8beea6386287c2044e9d69b8ee8
alexis <alexis.marchal@france-pari.org>
2019-11-12 09:13:56 +0100

    d

--- src/AppBundle/Command/ImportCdiscountCommand.php
+++ src/AppBundle/Command/ImportCdiscountCommand.php
@@ -134,6 +134,7 @@ class ImportCdiscountCommand extends Command
 
                 $jouet = $woocommerce->postProduct($data);
             }else{
+                var_dump(current($currentProduct));exit;
                 if ((string)$product->price->buynow <= current($currentProduct)->price){
                     if ((string)$product->price->productPriceOld && (string)$product->price->productPriceOld > 0){
                         $data['regular_price'] = (string)$product->price->productPriceOld;
@@ -144,7 +145,7 @@ class ImportCdiscountCommand extends Command
                     $data['external_url'] = (string)$product->uri->awTrack;
                 }
 
-                if (!current($currentProduct)->price ||current($currentProduct)->price == NULL){
+                if (!current($currentProduct)->sale_price ||current($currentProduct)->sale_price == NULL){
                     $data['sale_price'] = (string)$product->price->buynow;
                 }
                 if (!current($currentProduct)->external_url ||current($currentProduct)->external_url == NULL){
